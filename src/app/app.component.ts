2917b3c70f64a728eb3c07880178a78830a97edd
laurenchristofer <lauren.christofer@gmail.com>
2018-11-30 12:43:28 +0700

    Commit By lauren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
+import firebase from 'firebase';
+//import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'HomePage';
+  home = 'HomePage';
+  about = 'AboutPage';
+  signIn = 'SignInPage';
+  @ViewChild('sideMenu') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menuCtrl:MenuController) {
+    var config = {
+    apiKey: "AIzaSyBuircFO_sf_vcemKrwqWIqwXUGibtlUQc",
+    authDomain: "vendorpedia-54d53.firebaseapp.com",
+    databaseURL: "https://vendorpedia-54d53.firebaseio.com",
+    projectId: "vendorpedia-54d53",
+    storageBucket: "vendorpedia-54d53.appspot.com",
+    messagingSenderId: "947381527762"
+    };
+    firebase.initializeApp(config);
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +32,10 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  Load(page: string) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+    }
 }
 
---
+++ src/service/firbaseAuthService.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+export class AuthService{
+    signInUser(email:string, password:string){
+        return firebase.auth().signInWithEmailAndPassword(email,password);
+    }
+
+    registerUser(email:string, password:string){
+        return firebase.auth().createUserWithEmailAndPassword(email,password);
+    }
+
+    logout(){
+        return firebase.auth().signOut();
+    }
+
+    getCurrentUser(){
+        let cu = firebase.auth().currentUser;
+    }
+
+    getActiveUser(){
+        return firebase.auth().currentUser;
+    }
+}
