bc21ce0eb5339c3197667cc033daff82f8a5a86c
juliusHin <juliustanuwijaya.indonesian@gmail.com>
2018-12-07 13:56:35 +0700

    merge punya Julius dan Christofer
    Signed-off-by: juliusHin <juliustanuwijaya.indonesian@gmail.com>

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -3,12 +3,6 @@ import { User } from './../../data/user.interface';
 import { AuthService } from './../../service/firbaseAuthService';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
-=======
-// import {ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
->>>>>>> 81db7ac7dccd2ed70171648ad2d8f992964bc74e
 /**
  * Generated class for the SignUpPage page.
  *
@@ -22,60 +16,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: 'sign-up.html',
 })
 export class SignUpPage {
-<<<<<<< HEAD
   constructor(public navCtrl: NavController, public navParams: NavParams, private authServiceCtrl:AuthService) {
-=======
-
-  // registerForm = new FormGroup({
-  //   username: new FormControl()
-  // });
-  registerForm : FormGroup;
-  registerUser: any;
-  username: string= '';
-  fname: string='';
-  lname: string='';
-  email:string ='';
-  titleAlert: string = 'This field is required';
-
-  userType: any;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
-    this.registerForm = fb.group({
-      'username': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(25)])],
-      'fname': [null, Validators.compose([Validators.required])],
-      'lname': [null,],
-      'email': [null, Validators.compose([Validators.required, Validators.email])],
-      'validate': ''
-    });
->>>>>>> 81db7ac7dccd2ed70171648ad2d8f992964bc74e
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignUpPage');
   }
 
-<<<<<<< HEAD
   registerNewUser(f:any){
     this.authServiceCtrl.registerUser(f.value.email, f.value.password);
   }
 
-=======
-  onSubmit(registerUser){
-
-  }
-  ngOnInit() {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.registerForm.get('validate').valueChanges.subscribe(
-      (validate) =>{
-        if (validate == '1'){
-          this.registerForm.get('username').setValidators([Validators.required, Validators.minLength(6)]);
-        }else{
-          this.registerForm.get('username').setValidators([Validators.required]);
-        }
-        this.registerForm.get('username').updateValueAndValidity();
-      }
-    )
-  }
->>>>>>> 81db7ac7dccd2ed70171648ad2d8f992964bc74e
 }
