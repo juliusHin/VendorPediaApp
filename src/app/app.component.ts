ed01b182cec2c5d09eb999949b08b39956df1275
dainuri <dainuri@student.umn.ac.id>
2018-12-12 20:37:22 +0700

    coba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import firebase from 'firebase';
 //import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'HomePage';
+  rootPage:any = 'AboutPage';
   home = 'HomePage';
   about = 'AboutPage';
   signIn = 'SignInPage';
   @ViewChild('sideMenu') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menuCtrl:MenuController) {
+  constructor(platform: Platform, statusBar: StatusBar, private menuCtrl:MenuController) {
     var config = {
     apiKey: "AIzaSyBuircFO_sf_vcemKrwqWIqwXUGibtlUQc",
     authDomain: "vendorpedia-54d53.firebaseapp.com",
@@ -29,13 +28,12 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
     });
   }
 
-  Load(page: string) {
-    this.nav.setRoot(page);
-    this.menuCtrl.close();
-    }
+  // Load(page: string) {
+  //   this.nav.setRoot(page);
+  //   this.menuCtrl.close();
+  //   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
+// import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
 
 @NgModule({
   declarations: [
-    MyApp,
+    
+    AboutPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(AboutPage)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    AboutPage
   ],
   providers: [
     StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
